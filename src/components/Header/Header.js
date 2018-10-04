73dede428a9a2c07f8205381663c3fef50d3081b
Marquis Mitchell <marquis0403@gmail.com>
2018-10-04 13:42:51 -0400

    changed M icon to edited one

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
                 <div className="header__primary_logo"></div>
                 <ul className="header__nav_list">
                     {/* <li className="header__primary_logo"></li> */}
-                    <li><a>About Me</a></li>
-                    <li><a>Projects</a></li>
-                    <li><a>Contact</a></li>
+                    <li className="header__nav_listitem"><a href="#aboutme">About Me</a></li>
+                    <li className="header__nav_listitem"><a href="#projects">Projects</a></li>
+                    <li className="header__nav_listitem"><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
