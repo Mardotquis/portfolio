e84d760fcad812fee0e894ff1dd76c03b5ce57ba
Marquis Mitchell <marquis0403@gmail.com>
2018-10-04 09:56:30 -0400

    flipped color scheme and changed font

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import "./App.scss";
 
 import Header from './components/Header/Header';
+import MainHeader from './components/MainHero/MainHero';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <MainHeader />
       </div>
     );
   }
---
+++ src/components/MainHero/MainHero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainHero = () => {
+    return (
+        <div className="mainhero">
+            <h1 className="main_primary_text">Hello, I'm Marquis!</h1>
+            <p className="main_secondary_text"><span className="main_open-brace">&#123;</span>Aspiring Web Developer<span className="main_close-brace">&#125;</span></p>
+            <ul></ul>
+        </div>
+    )
+};
+
+export default MainHero;
