7245a76f2c1da7e5ece6c55e511c3568133a30ab
Marquis Mitchell <marquis0403@gmail.com>
2018-10-05 14:49:46 -0400

    finished slanted div design for products, fixed header padding issue

--- src/components/Projects/ProjectsCard.js
+++ src/components/Projects/ProjectsCard.js
@@ -3,7 +3,12 @@ import React from 'react';
 const ProjectsCard = () => {
     return (
         <div className="projects__card">
+            <div className="card__left-section">
+                <figure></figure>
+            </div>
+            <div className="card__right-section">
 
+            </div>
         </div>
     )
 };
