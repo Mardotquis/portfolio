542141c581ec7e9ee33359288c52043926eef1bb
Marquis Mitchell <marquis0403@gmail.com>
2018-10-06 22:15:45 -0400

    finished with some contact form styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,14 +4,13 @@ const Contact = () => {
 
     const submitForm = (e) => {
         e.preventDefault();
-        const firstName = document.querySelector('#firstName').value;
-        const lastName = document.querySelector('#lastName').value;
-        const phoneNumber = document.querySelector('#phoneNumber').value;
-        const guestEmail = document.querySelector('#guestEmail').value;
-        const guestMessage = document.querySelector('#guestMessage').value;
-        const bodyBeingSent = { firstName, lastName, phoneNumber, guestEmail, guestMessage }
+        const firstName = document.querySelector('#firstName').value
+            , lastName = document.querySelector('#lastName').value
+            , phoneNumber = document.querySelector('#phoneNumber').value
+            , guestEmail = document.querySelector('#guestEmail').value
+            , guestMessage = document.querySelector('#guestMessage').value
+            , bodyBeingSent = { firstName, lastName, phoneNumber, guestEmail, guestMessage };
         console.log(bodyBeingSent);
-
         fetch("https://marquis-portfolio.herokuapp.com/", {
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
@@ -25,24 +24,32 @@ const Contact = () => {
     };
     return (
         <section id="contact">
-            <h1 className="section__header__primary">Contact</h1>
-            <form onSubmit={submitForm}>
-                <label htmlFor="firstName"></label>
-                <input type="text" name="firstName" id="firstName" />
+            <h2 className="section__header__primary">Contact Me</h2>
+            <div className="contact__main-content">
 
-                <label htmlFor="lastName"></label>
-                <input type="text" name="lastName" id="lastName" />
 
-                <label htmlFor="phoneNumber"></label>
-                <input type="text" name="phoneNumber" id="phoneNumber" />
+                <a href="mailto:marquis0403@gmail.com" className="contact__myEmail">Marquis0403@gmail.com</a>
 
-                <label htmlFor="guestEmail"></label>
-                <input type="text" name="guestEmail" id="guestEmail" />
+                <form onSubmit={submitForm} className="contact__form" required >
+                    <div className="contact__form-names">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input type="text" name="firstName" id="firstName" className="contactInputField" placeholder="First Name" />
 
-                <label htmlFor="guestMessage"></label>
-                <input type="text" name="guestMessage" id="guestMessage" />
-                <input type="submit" />
-            </form>
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input type="text" name="lastName" id="lastName" className="contactInputField" placeholder="Last Name" />
+                        <label htmlFor="phoneNumber">Phone Number</label>
+                        <input type="tel" name="phoneNumber" id="phoneNumber" className="contactInputField" placeholder="Phone" />
+
+                    </div>
+
+                    <label htmlFor="guestEmail">Email:</label>
+                    <input type="email" name="guestEmail" id="guestEmail" className="contactInputField" placeholder="Email" />
+
+                    <label htmlFor="guestMessage">Message:</label>
+                    <textarea type="text" name="guestMessage" id="guestMessage" className="contactInputField" placeholder="Message" />
+                    <input type="submit" className="contactSubmitButton" />
+                </form>
+            </div>
         </section>
     )
 };
