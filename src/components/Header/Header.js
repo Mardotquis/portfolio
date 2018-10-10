fdb57bb624e2b4ed74ee95a4efd5d71c7ab142ef
Marquis Mitchell <marquis0403@gmail.com>
2018-10-10 16:22:04 -0400

    added hamburger and it works

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Header = () => {
+    const toggleHiddenMenu=()=>{
+        const navMenu = document.querySelector('.header__hiddenMenu');
+        if(navMenu.style.display === "none"){
+            navMenu.style.display = "flex";
+        }else{
+            navMenu.style.display = "none"
+        }
+    }
     return (
         <header>
             <nav className="header__nav">
@@ -10,8 +18,17 @@ const Header = () => {
                     <li className="header__nav_listitem"><a href="#aboutme">About Me</a></li>
                     <li className="header__nav_listitem"><a href="#contact">Contact</a></li>
                 </ul>
-                {/* <div className="header--menuicon"></div> */}
+
+
             </nav>
+            <div className="header--menuicon" onClick={toggleHiddenMenu}></div>
+            <nav className="header__nav--hidden">
+            <ul className="header__hiddenMenu">
+                    <li className="header__nav_listitem"><a href="#projects" onClick={toggleHiddenMenu}>Projects</a></li>
+                    <li className="header__nav_listitem"><a href="#aboutme" onClick={toggleHiddenMenu}>About Me</a></li>
+                    <li className="header__nav_listitem"><a href="#contact" onClick={toggleHiddenMenu}>Contact</a></li>
+                </ul>
+                </nav>
         </header>
     )
 };
