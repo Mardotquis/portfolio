c2590d9ce4453833950f2b9bd54f310ec3bc1c00
Marquis Mitchell <marquis0403@gmail.com>
2018-10-10 21:17:05 -0400

    works??

--- src/components/MainHero/MainHero.js
+++ src/components/MainHero/MainHero.js
@@ -6,7 +6,7 @@ const MainHero = (props) => {
     console.log(props.mainHeroIcons)
     return (
         <div className="mainhero" id="home">
-            <h1 className="main_primary_text ">Hello, I'm Marquis!</h1>
+            <h1 className="main_primary_text ">Hello, I'm Marquis!!!</h1>
             <p className="main_secondary_text"><span className="main_open-brace">&#123;</span>Full Stack Web Developer<span
                 className="main_close-brace">&#125;</span></p>
             <ul className="mainhero__icon-container">
