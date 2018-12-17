1e3285172fdb0b167134bcf1aa575a82c01a83c9
Marquis Mitchell <mamitchell@c02v21yxg8wl.redventures.net>
2018-12-17 08:33:29 -0500

    updated title and body of about me paragraph to include RV

--- src/components/MainHero/MainHero.js
+++ src/components/MainHero/MainHero.js
@@ -4,7 +4,7 @@ const MainHero = (props) => {
     return (
         <div className="mainhero" id="home">
             <h1 className="main_primary_text ">Hello, I'm Marquis!</h1>
-            <p className="main_secondary_text"><span className="main_open-brace">&#123;</span>Full Stack Web Developer<span
+            <p className="main_secondary_text"><span className="main_open-brace">&#123;</span>Software Engineer<span
                 className="main_close-brace">&#125;</span></p>
             <ul className="mainhero__icon-container">
                 {props.mainHeroIcons.map(icon=>{
