05163336f3779634bf42b64a8cc3813d0d9d1c23
Marquis L. Mitchell <marquis0403@gmail.com>
2019-04-17 08:36:51 -0400

    added a few rel attributes to help prevent cross site scripting

--- src/components/MainHero/MainHeroIcons.js
+++ src/components/MainHero/MainHeroIcons.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const MainHeroIcons = (props) => {
 
-    const {iconPath, iconLink } = props;
+    const { iconPath, iconLink } = props;
     const grabPath = { backgroundImage: `url(${iconPath})` };
     return (
-        <a href={iconLink} className="mainhero__icons" style={grabPath} target="_blank"></a>
+        <a href={iconLink} className="mainhero__icons" style={grabPath} target="_blank" rel="noopener"></a>
     )
 };
 
